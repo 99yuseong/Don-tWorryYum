22c4c9ea92d4749fbce96f3c21c75732a95802a2
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-08-10 12:04:54 +0900

    Create Xcode Project

---
+++ DontWorryYum/DontWorryYum/DontWorryYumApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontWorryYumApp.swift
+//  DontWorryYum
+//
+//  Created by 남유성 on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DontWorryYumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
